9323fe58465d73f4fec78016c7eba7665e1756eb
nadir <nadirsellains@gmail.comgit>
2025-02-18 18:56:05 +0100

    Amélioration de l'interface et intégration des données de mapping

--- main.py
+++ main.py
@@ -82,17 +82,17 @@ SHAPE_MAPPING = {
 }
 
 CLARITY_MAPPING = {
-    'FL': 'Flawless',
-    'IF': 'Internally Flawless',
-    'VVS1': 'Very Very Slightly Included 1',
-    'VVS2': 'Very Very Slightly Included 2',
-    'VS1': 'Very Slightly Included 1',
-    'VS2': 'Very Slightly Included 2',
-    'SI1': 'Slightly Included 1',
-    'SI2': 'Slightly Included 2',
-    'I1': 'Included 1',
-    'I2': 'Included 2',
-    'I3': 'Included 3'
+    'FL': 'FL',
+    'IF': 'IF',
+    'VVS1': 'VVS1',
+    'VVS2': 'VVS2',
+    'VS1': 'VS1',
+    'VS2': 'VS2',
+    'SI1': 'SI1',
+    'SI2': 'SI2',
+    'I1': 'I1',
+    'I2': 'I2',
+    'I3': 'I3'
 }
 
 # Sort clarity codes by length to avoid confusion (e.g., "SI2" vs "I2")
